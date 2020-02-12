71f6087a45b830298251729e9f713c26eaf453e0
Faris Aziz <farisaziz12@gmail.com>
2020-02-12 15:07:31 +0000

    workout edit cards css redesigned and fixed

--- src/CoachComponents.js/AthleteCard.js
+++ src/CoachComponents.js/AthleteCard.js
@@ -36,23 +36,3 @@ class AthleteCard extends Component {
 }
 
 export default AthleteCard;
-            // <Card>
-            //     <Card.Content>
-            //         <Card.Header>{first_name + " " + last_name}</Card.Header>
-            //         <Card.Meta>Age: </Card.Meta>
-            //         <Card.Description>
-            //         Date of last workout assigned: 
-            //         </Card.Description>
-            //     </Card.Content>
-            //     <Card.Content extra>
-            //         <div className='ui two buttons'>
-            //         <Button onClick={() => this.props.handleAthlete(id)} basic color='green'>
-            //             Assign Workout
-            //         </Button>
-            //         <Button basic color='red' >
-            //             <a href={`mailto:${email}`}>Contact</a>
-            //         </Button>
-            //         </div> <br/><br/>
-            //         <Link exact to='/show/athlete'><Button onClick={() => this.props.handleAthleteShow(id)}>More</Button></Link>
-            //     </Card.Content>
-            // </Card>
--- src/CoachComponents.js/WorkoutCard.js
+++ src/CoachComponents.js/WorkoutCard.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Button, Card } from 'semantic-ui-react'
+import './WorkoutCard.css'
 
 
 class WorkoutCard extends Component {
-    state = {  }
+    state = {
+        cardClass: "movement-card"
+    }
+
+    handleClick = () => {
+        this.setState({cardClass: this.state.cardClass === "movement-card"? "movement-card flipped" : "movement-card"})
+    }
 
     handleDelete = () => {
          fetch(`https://wodshare.herokuapp.com/workouts/delete/${this.props.id}`, {
@@ -12,26 +19,48 @@ class WorkoutCard extends Component {
     }
 
     render() {
-        const { name, date, id } = this.props
+        const { name, date, id, workout, category, score } = this.props
         return (
-        <Card>
-            <Card.Content>
-                <Card.Header>{name}</Card.Header>
-                <Card.Meta>Date: {date}</Card.Meta>
-            </Card.Content>
-            <Card.Content extra>
-                <div className='ui two buttons'>
-                <Button onClick={() => this.props.handleSelectWorkout(id)} basic color='green'>
-                    Edit
-                </Button>
-                <Button onClick={this.handleDelete} basic color='red' >
-                    Delete
-                </Button>
-                </div> 
-            </Card.Content>
-        </Card>
+            <>
+                <div className="workout-card">
+                    <h2 className="workout-title">"{name}"</h2>
+                    <h3 className="workout-date"><i>{date}</i></h3>
+                    <div className="workout-bar">
+                    <h4 className="card-content">Score: {score}</h4>
+                    <h3 className="card-content">Category: {category}</h3>
+                    {/* <h4 className="card-content">Workout: </h4> */}
+                    {workout.split('\n').map(element => {
+                        return <h5 className="card-content wod-text">{element}</h5>
+                    })}
+                    <div className='wod-buttons'>
+                    <Button onClick={() => this.props.handleSelectWorkout(id)} basic color='green'>
+                         Edit
+                     </Button>
+                     <Button onClick={this.handleDelete} basic color='red' >
+                         Delete
+                     </Button>
+                     </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
 
 export default WorkoutCard;
+        // <Card>
+        //     <Card.Content>
+        //         <Card.Header>{name}</Card.Header>
+        //         <Card.Meta>Date: {date}</Card.Meta>
+        //     </Card.Content>
+        //     <Card.Content extra>
+        //         <div className='ui two buttons'>
+        //         <Button onClick={() => this.props.handleSelectWorkout(id)} basic color='green'>
+        //             Edit
+        //         </Button>
+        //         <Button onClick={this.handleDelete} basic color='red' >
+        //             Delete
+        //         </Button>
+        //         </div> 
+        //     </Card.Content>
+        // </Card>
